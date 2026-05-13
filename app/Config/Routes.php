0998daefb00fc01b14c55c4ba0afb09b2fa3f30b
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-05-13 11:08:43 +0300

    fix{route}

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+$routes->get('/',              'AuthController::loginPage');
+$routes->get('login',           'AuthController::loginPage');
+$routes->post('login',          'AuthController::login');
+
+
 $routes->group('rh', ['namespace' => 'App\Controllers\RH'], function ($routes) {
     $routes->get('demandes', 'RhController::demandes');
     $routes->get('approuver/(:num)', 'RhController::approuver/$1');
     $routes->post('refuser/(:num)', 'RhController::refuser/$1');
 });
+
