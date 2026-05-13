e5396f030632bb0c333eb4b49a10e4678c4e1d7f
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-05-13 13:03:31 +0300

    fix{type}

--- app/Models/TypeCongeModel.php
+++ app/Models/TypeCongeModel.php
@@ -1,16 +1,9 @@
 <?php
-<<<<<<< HEAD
 namespace App\Models;
-=======
-
-namespace App\Models;
-
->>>>>>> ad0622f9a1f2abaa434908eabe9528ba9b0c3c0e
 use CodeIgniter\Model;
 
 class TypeCongeModel extends Model
 {
-<<<<<<< HEAD
     protected $table         = 'types_conge';
     protected $primaryKey    = 'id';
     protected $useTimestamps = false;
@@ -30,47 +23,3 @@ class TypeCongeModel extends Model
             ->getResultArray();
     }
 }
-=======
-    protected $table            = 'typeconges';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields = [
-    'libelle',
-    'jours_annuels',
-    'deductible',
-];
-
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
-}
->>>>>>> ad0622f9a1f2abaa434908eabe9528ba9b0c3c0e
