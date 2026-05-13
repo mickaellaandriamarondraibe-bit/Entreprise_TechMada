1866b83f058b0969123b1760e7eb42b8751a06b4
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-05-13 11:56:18 +0300

    feat'liste demandes}

--- app/Controllers/Employe/CongeController.php
+++ app/Controllers/Employe/CongeController.php
@@ -10,14 +10,10 @@ class CongeController extends BaseController
     public function index()
     {
         $employe_id = session()->get('employe_id');
+        $congeModel = new CongeModel();
 
-            $soldeModel = new SoldeModel();
-            $congeModel = new CongeModel();
-
-            return view('employe/conge/demandes', [
-                'soldes'             => $soldeModel->getSoldesAvecType($employe_id),
-                'stats'              => $congeModel->getStats($employe_id),
-                'dernieres_demandes' => $congeModel->getDernieresDemandes($employe_id),
-            ]);
+        return view('employe/conge/demands', [
+            'demandes' => $congeModel->getDemandesEmploye($employe_id),
+        ]);
     }
 }
