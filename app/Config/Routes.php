90effc423558d30ba802bb51f2cb485cf665353f
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-05-13 10:24:53 +0300

    creation model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,3 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+$routes->get('/',              'AuthController::loginPage');
+$routes->get('login',           'AuthController::loginPage');
+$routes->post('login',          'AuthController::login');
+
---
+++ app/Controllers/Employe/CongeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Employe;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class CongeController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
